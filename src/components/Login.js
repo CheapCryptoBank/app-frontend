5d844a76af60916d47463314de97c3e0c43bb1a9
Linus Tickles <143286251+linus-tickles@users.noreply.github.com>
2023-08-27 00:59:56 +0000

    updated URLs

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,14 +12,14 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import image from "../images/NiftyBank.png";
+import image from "../images/cheapcryptobank.png";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
-        Niftybank
+        CheapCryptoBank
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
